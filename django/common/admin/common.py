3978f1016e19300dbdb605b08c06ac7de580e84a
goct8 <goct@naver.com>
2025-05-13 18:49:21 +0900

    FEAT: Stored file preview 와 link 추가

--- django/common/admin/common.py
+++ django/common/admin/common.py
@@ -1,5 +1,7 @@
 from datetime import datetime, timedelta
 from django.contrib import admin
+from django.utils.html import format_html
+from core.environment import env
 from common.models import (
     StoredFile,
     StoredFilesGroup
@@ -14,10 +16,28 @@ from common.enums import UploaderTypeEnum
 class StoredFileAdmin(admin.ModelAdmin):
     form = StoredFileAdminForm
 
-    def get_list_display(self, request):
-        return tuple(field.name for field in self.model._meta.fields)
+    # def get_list_display(self, request):
+    #     default = tuple(field.name for field in self.model._meta.fields)
+    #     return default + ("preview",)
 
-    list_display_links = ("name",)
+    list_display = (
+        "id",
+        "preview",
+        "name",
+        "ext",
+        "download_link",
+        # "path",
+        "status",
+        "hash",
+        "uploader_type",
+        "uploader_id",
+        "stored_files_group",
+        "expire_at",
+        "created_at",
+        "updated_at",
+    )
+
+    list_display_links = ("id", "name",)
     search_fields = ("name", "hash")
     list_filter = ("uploader_type", "status")
     ordering = ("-created_at",)
@@ -27,6 +47,24 @@ class StoredFileAdmin(admin.ModelAdmin):
         obj.uploader_type = UploaderTypeEnum.ADMIN.value
         obj.uploader_id = request.user.id
         return super().save_model(request, obj, form, change)
+    
+    def preview(self, obj):
+        if obj.ext.lower() in [".png", ".jpg", ".jpeg", ".gif"]:
+            return format_html(
+                """<img src="{origin}/{path}" style="width:150px; height:150px;"/>""",
+                origin=env.get("ORIGIN", "http://localhost:8000"),
+                path=obj.path
+            )
+        return "No Preview"
+    preview.short_description = "Preview"
+    
+    def download_link(self, obj):
+        return format_html(
+            """<a href="{origin}/{path}">{path}</a>""",
+            origin=env.get("ORIGIN", "http://localhost:8000"),
+            path=obj.path
+        )
+    download_link.short_description = "Download Link"
 
 
 @admin.register(StoredFilesGroup)
