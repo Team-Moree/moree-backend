0d8dac4730b57aec0f3e72ac90529089d136f65c
goct8 <goct@naver.com>
2025-05-13 20:38:01 +0900

    FEAT: Character 모델에 __str__ 메서드 추가하여 객체 문자열 표현 정의

--- django/sample/models/character.py
+++ django/sample/models/character.py
@@ -25,6 +25,9 @@ class Character(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         verbose_name = _("Character")
         verbose_name_plural = _("Characters")
