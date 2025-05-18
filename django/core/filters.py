2d0e1452e7d3e5044a0aea4c2b9d22ab937e05a4
goct8 <goct@naver.com>
2025-05-19 00:21:13 +0900

    FIX: EpochTimeFilter에서 None 체크 로직 개선

--- django/core/filters.py
+++ django/core/filters.py
@@ -5,7 +5,7 @@ from rest_framework.exceptions import ValidationError
 
 class EpochTimeFilter(rest_framework.Filter):
     def filter(self, qs, value):
-        if value is None:
+        if value is None or value == "":
             return qs
         try:
             timestamp = float(value)
