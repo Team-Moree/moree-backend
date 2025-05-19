e7e932d8557222092562d372edd7e37d3704977d
goct8 <goct@naver.com>
2025-05-19 16:29:18 +0900

    FEAT: 테스트용 임시 사용자 추가

--- django/moree/permissions/user.py
+++ django/moree/permissions/user.py
@@ -1,9 +1,14 @@
 from rest_framework.permissions import BasePermission
+from moree.models import User
 
 
 class UserPermission(BasePermission):
 
     def has_permission(self, request, view):
+        # TODO: 추후 provider login-in 구현
+        request.user = User.objects.get(id=1)
+        return True
+
         authorization_header = request.headers.get("Authorization")
 
         if authorization_header is None:
