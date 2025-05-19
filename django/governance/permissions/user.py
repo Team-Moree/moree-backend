4bca2b6678cc74d2a5a52ba4e302bb31eaa51d39
goct8 <goct@naver.com>
2025-05-19 18:41:21 +0900

    REFACT:  governance 삭제

--- django/governance/permissions/user.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-
-class UserPermission(BasePermission):
-
-    def has_permission(self, request, view):
-        admin_session_id = request.COOKIES.get("admin-session-id")
-        return False
